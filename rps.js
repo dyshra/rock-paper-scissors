0b464d90d1f97cf382c3998dece7aa774c5ae50a
Shraddha <shraddha3m@gmail.com>
2021-08-05 10:17:15 +0530

    added buttons and a div to display result

--- rps.js
+++ rps.js
@@ -9,102 +9,76 @@ function computerPlay()
         return "Scissors";
 }
 
-function singleRound(playerSelection, computerSelection)
+function singleRound(e)
 {
     let rockReg=/rock/i;
     let paperReg=/paper/i;
     let scissorsReg=/scissors/i;
+    let computerSelection = computerPlay();
+    const result= document.getElementById('result');
 
-    if (rockReg.test(playerSelection))
+    if (rockReg.test(e.target.id))
     {
         if (computerSelection === "Rock")
         {   
-            console.log("It's a Tie!");
+            result.innerText = "It's a Tie!";
             return 0;
         }
         else if (computerSelection === "Paper")
         {
-            console.log("You lose! Paper beats Rock");
+            result.innerText = "You lose! Paper beats Rock";
             return -1;
         }
         else if (computerSelection === "Scissors")
         {
-            console.log("You win! Rock beats Scissors");
+            result.innerText = "You win! Rock beats Scissors";
             return 1;
         }
     }
 
-    else if (paperReg.test(playerSelection))
+    else if (paperReg.test(e.target.id))
     {
         if (computerSelection === "Rock")
         {    
-            console.log("You win! Paper beats Rock");
+            result.innerText = "You win! Paper beats Rock";
             return 1;
         }        
         else if (computerSelection === "Paper")
         {            
-            console.log("It's a Tie!");
+            result.innerText = "It's a Tie!";
             return 0;
         }        
         else if (computerSelection === "Scissors")
         {                
-            console.log("You lose! Scissors beat Paper");
+            result.innerText = "You lose! Scissors beat Paper";
             return -1;
         }    
     }
 
-    else if (scissorsReg.test(playerSelection))
+    else if (scissorsReg.test(e.target.id))
     {
         if (computerSelection === "Rock")
         {            
-            console.log("You lose! Rock beats Scissors");
+            result.innerText = "You lose! Rock beats Scissors";
             return -1;
         }        
         else if (computerSelection === "Paper")
         {            
-            console.log("You win! Scissors beat Paper");
+            result.innerText = "You win! Scissors beat Paper";
             return 1;
         }        
         else if (computerSelection === "Scissors")
         {            
-            console.log("It's a Tie!");
+            result.innerText = "It's a Tie!";
             return 0;
         }    
     }
     else
     {
-        console.log("Please enter one of the options");
+        result.innerText = "Please enter one of the options";
         return -2;
     }
 }
 
-function game()
-{
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 1; i <= 5; i++)
-    {
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-
-        let computerSelection = computerPlay();
-        let result = singleRound(playerSelection, computerSelection);
-
-        if (result === 1)
-            ++playerScore;
-        else if (result === -1)
-            ++computerScore;
-        else if (result === -2)
-            singleRound();
-
-        console.log("Your score: "+playerScore);
-        console.log("Computer's score: "+computerScore);
-    }
-
-    if (playerScore > computerScore)
-        console.log("You win this game! ");
-    else
-        console.log("You lost this game :(");
-}
-
-game();
+const button_arr = document.querySelectorAll('button')
+button_arr.forEach(button => button.addEventListener('click', singleRound))
